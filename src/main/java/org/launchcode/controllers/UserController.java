a455ae77511a41d0ff7711cb4d3857241f1e1207
Dai <dai.dickerson@gmail.com>
2023-10-06 00:47:55 -0500

    added code to UserController

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,7 +1,11 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +17,17 @@ public class UserController {
         return "/user/add";
     }
 
+    @PostMapping
+    public String processAddUserFrom (Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+        //model.addAttribute("username", user.getUsername());
+        if(user.getPassword().equals(verify)) {
+            return "user/index";
+        } else {
+            model.addAttribute("error", "Passwords do not match");
+            return "user/add";
+        }
+    }
 
 }
